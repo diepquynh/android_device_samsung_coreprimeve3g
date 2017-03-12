diepquynh/android_device_samsung_grandprimeve3g

device: Remove "device_" prefix
Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

AndroidProducts.mk
@@ -18,4 +18,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/device_grandprimeve3g.mk
+	$(LOCAL_DIR)/grandprimeve3g.mk

diepquynh/android_device_samsung_coreprimeve3g

AndroidProducts.mk
@@ -18,4 +18,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/device_coreprimeve3g.mk
+	$(LOCAL_DIR)/coreprimeve3g.mk