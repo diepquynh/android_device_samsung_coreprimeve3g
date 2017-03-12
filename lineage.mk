diepquynh/android_device_samsung_grandprimeve3g

cm: Rebrand to Lineage
Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

lineage.mk
@@ -5,14 +5,14 @@ $(call inherit-product, vendor/cm/config/telephony.mk)
 PRODUCT_RELEASE_NAME := grandprimeve3g
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_grandprimeve3g.mk)
+$(call inherit-product, $(LOCAL_PATH)/grandprimeve3g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_NAME := cm_grandprimeve3g
+PRODUCT_NAME := lineage_grandprimeve3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G531H
 PRODUCT_MANUFACTURER := samsung

diepquynh/android_device_samsung_coreprimeve3g

lineage.mk
@@ -4,11 +4,11 @@ $(call inherit-product, vendor/cm/config/telephony.mk)
 # Release name
 PRODUCT_RELEASE_NAME := coreprimeve3g
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_coreprimeve3g.mk)
+$(call inherit-product, $(LOCAL_PATH)/coreprimeve3g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := coreprimeve3g