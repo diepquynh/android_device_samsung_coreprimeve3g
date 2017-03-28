f7cd7eb10895a2da71e326c5426b67913c375762
Remilia Scarlet <remilia.1505@gmail.com>
2017-03-28 14:24:38 +0700

    ramdisk: Move all init files to common
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- coreprimeve3g.mk
+++ coreprimeve3g.mk
@@ -34,13 +34,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	media_profiles.xml
 
-# Rootdir files
-PRODUCT_PACKAGES += \
-	init.sc8830.rc \
-	init.sc8830.usb.rc \
-	init.grandprimeve3g_base.rc \
-	ueventd.sc8830.rc
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_coreprimeve3g
 PRODUCT_DEVICE := coreprimeve3g
