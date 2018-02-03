a4df1fa26cb4533dd2988cab0c433056206837a9
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-03 18:09:38 +0000

    bluetooth: Move to scx35-common
    Change-Id: I6eaf1a4b001c08d49cfaf38c17e6c4d991004d8d
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,9 +49,6 @@ WIFI_DRIVER_NVRAM_PATH := "/system/etc/wifi/nvram_net.txt"
 WIFI_BAND := 802_11_ABG
 BOARD_HAVE_SAMSUNG_WIFI := true
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/coreprimeve3g/bluetooth
-
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogen_coreprimeve3g_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/coreprimeve3g
